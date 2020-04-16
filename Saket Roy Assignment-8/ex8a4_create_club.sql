1e7dd6f80db2d0299e7503d1fd5e2a3ba2f44e5f
Saket Roy <roy05@live.missouristate.edu>
2020-04-16 11:22:21 +0530

    Create .psp pages to insert to db through forms

--- Saket Roy Assignment-8/ex8a4_create_club.sql
+++ Saket Roy Assignment-8/ex8a4_create_club.sql
@@ -1,14 +1,23 @@
-create or replace procedure ex8a4_create_club as
-begin
+create or replace procedure ex8a4_create_club(
+    name in varchar2, 
+    street in varchar2, 
+    city in varchar2, 
+    state in varchar2, 
+    zip in number) as
 
-insert into sporting_clubs (club_id, name, street, city, state, zip)
-values (
-    club_sequence.nextval,
-    'Saket''s Awesome Club',
-    '78, Rajdanga Nabapally',
-    'Kolkata',
-    'WB',
-    70010);
-    
+club_name_text sporting_clubs.name%type;
+club_id_text sporting_clubs.club_id%type;
+begin
+    insert into sporting_clubs (club_id, name, street, city, state, zip)
+    values (
+        club_sequence.nextval,
+        name,
+        street,
+        city,
+        state,
+        zip);
+    select club_sequence.currval into club_id_text from dual;
+    select name into club_name_text from sporting_clubs where club_id = club_id_text;
+    dbms_output.put_line('New Club ' || club_name_text || ' having ID ' || club_id_text || ' successfully created!');  
 end;
     
